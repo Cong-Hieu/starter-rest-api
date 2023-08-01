34127400127caa5f98217744c59969482a9537de
Hieu Truong <hieu.tnxd.bk@gmail.com>
2023-08-01 21:43:45 +0700

    'commit'

--- src/services/chatService.js
+++ src/services/chatService.js
@@ -4,7 +4,7 @@ const moment = require('moment')
 const { separateArrayByIndex } = require('../utils/helper')
 const { chatList, s3 } = dbModel
 
-const numberLoadItem = 20
+const numberLoadItem = 10
 const mbSize = Math.pow(1024, 2)
 
 const updateNoteInMockApi = async (result) => {
